bf600de12b587bc101682ab5b272e9a5ff8ae6bb
lyemoml <coderfee@outlook.com>
2019-12-14 22:14:23 +0800

    Fix seo title

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -5,7 +5,7 @@ import SEO from '../components/seo';
 export default function About() {
   return (
     <Layout>
-      <SEO></SEO>
+      <SEO title="关于" description="关于 CODERFEE 前端开发"></SEO>
       <div style={{ minHeight: '100vh', padding: '2rem' }}>
         <h1>Coming soon...</h1>
       </div>
