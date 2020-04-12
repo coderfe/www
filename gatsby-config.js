0d37bf4ca019f6c45db5df12d35297b4345c9329
chendongdong <coderfee@outlook.com>
2020-04-12 09:51:52 +0800

    Change favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -102,7 +102,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#8a2d22`,
         display: `standalone`,
-        icon: `src/images/icon.jpg`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     {
