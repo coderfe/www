bc166937a690681beca57fdab4cdbaceb17205ea
coderfee <coderfee@outlook.com>
2021-11-06 22:32:26 +0800

    feat: about page

--- src/components/provider/index.tsx
+++ src/components/provider/index.tsx
@@ -5,7 +5,7 @@ import github from 'prism-react-renderer/themes/github'
 import useDarkMode from 'use-dark-mode'
 
 const H1 = (props: any) => <h1 className="text-2xl" {...props} />
-const H2 = (props: any) => (
+export const H2 = (props: any) => (
   <h2 className="relative space-x-4 text-lg mt-8 mb-3" {...props}>
     <span className="absolute left-0 bottom-1 top-1 block w-1 rounded-sm bg-gray-700 font-semibold dark:bg-gray-50" />
     <span className="text-gray-700 dark:text-gray-50">{props.children}</span>
@@ -15,14 +15,14 @@ const H3 = (props: any) => <h3 className="font-semibold" {...props} />
 const Paragraph = (props: any) => (
   <p className="text-base leading-7 my-3" {...props} />
 )
-const UL = (props: any) => (
+export const UL = (props: any) => (
   <ul style={{ listStyleType: 'circle' }} className="my-3 ml-10" {...props} />
 )
 const OL = (props: any) => (
   <ol style={{ listStyleType: 'auto' }} className="my-3 ml-10" {...props} />
 )
-const LI = (props: any) => <li className="leading-7" {...props} />
-const A = (props: any) => (
+export const LI = (props: any) => <li className="leading-7" {...props} />
+export const A = (props: any) => (
   <a
     className="border-b-2 border-gray-700 text-gray-800 transition-colors duration-75 hover:text-gray-900 dark:text-gray-100 dark:hover:text-gray-50 dark:border-gray-50"
     {...props}
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,14 +1,65 @@
 import * as React from 'react'
 import Container from '../components/layout/Container'
 import { Layout } from '../components/layout/Layout'
+import { A, H2, LI, UL } from '../components/provider'
 import { SEO } from '../components/SEO'
 
 const AboutPage = () => {
   return (
     <Layout>
-      <SEO />
-      <Container className="h-full grid gap-8 py-5">
-        <h2>About Me</h2>
+      <SEO subTitle="about coderfee" />
+      <Container className="h-screen py-5 text-gray-700 dark:text-gray-100">
+        <H2>About Me</H2>
+        <p>前端开发一枚，目前在职京东物流</p>
+
+        <H2>macOS setup</H2>
+        <UL>
+          <LI>Visual Studio Code</LI>
+          <LI>WebStorm</LI>
+          <LI>iTerm2</LI>
+        </UL>
+
+        <H2>Windows setup</H2>
+        <UL>
+          <LI>Visual Studio Code</LI>
+          <LI>WebStorm</LI>
+          <LI>Windows Terminal & Oh My Posh</LI>
+        </UL>
+
+        <H2>Projects</H2>
+        <UL>
+          <LI>
+            <A href="https://marketplace.visualstudio.com/publishers/coderfee">
+              VSCode 插件集
+            </A>
+          </LI>
+          <LI>
+            <A href="https://coderfee.com" target="__blank">
+              CSpace
+            </A>
+            ——个人网站
+          </LI>
+          <LI>
+            <A href="https://github.com/coderfe/db-book" target="__blank">
+              db-book
+            </A>
+            ——爬取豆瓣读书标记过的书
+          </LI>
+        </UL>
+
+        <H2>Links</H2>
+        <UL>
+          <LI>
+            <A href="https://github.com/coderfe" target="__blank">
+              GitHub
+            </A>
+          </LI>
+          <LI>
+            <A href="https://twitter.com/coderfee" target="__blank">
+              Twitter
+            </A>
+          </LI>
+        </UL>
       </Container>
     </Layout>
   )
