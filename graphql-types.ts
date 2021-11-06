789f1dfa7151993160881b434548eb19db8efb03
coderfee <coderfee@outlook.com>
2021-11-06 20:17:38 +0800

    fix: transition

--- graphql-types.ts
+++ graphql-types.ts
@@ -256,6 +256,8 @@ export type DirectoryCtimeArgs = {
 export type Site = Node & {
   buildTime?: Maybe<Scalars['Date']>;
   siteMetadata?: Maybe<SiteSiteMetadata>;
+  port?: Maybe<Scalars['Int']>;
+  host?: Maybe<Scalars['String']>;
   polyfill?: Maybe<Scalars['Boolean']>;
   pathPrefix?: Maybe<Scalars['String']>;
   id: Scalars['ID'];
@@ -879,6 +881,8 @@ export type QueryAllDirectoryArgs = {
 export type QuerySiteArgs = {
   buildTime?: Maybe<DateQueryOperatorInput>;
   siteMetadata?: Maybe<SiteSiteMetadataFilterInput>;
+  port?: Maybe<IntQueryOperatorInput>;
+  host?: Maybe<StringQueryOperatorInput>;
   polyfill?: Maybe<BooleanQueryOperatorInput>;
   pathPrefix?: Maybe<StringQueryOperatorInput>;
   id?: Maybe<StringQueryOperatorInput>;
@@ -2083,6 +2087,8 @@ export type SiteFieldsEnum =
   | 'siteMetadata___siteUrl'
   | 'siteMetadata___twitterUsername'
   | 'siteMetadata___author'
+  | 'port'
+  | 'host'
   | 'polyfill'
   | 'pathPrefix'
   | 'id'
@@ -2216,6 +2222,8 @@ export type SiteGroupConnectionGroupArgs = {
 export type SiteFilterInput = {
   buildTime?: Maybe<DateQueryOperatorInput>;
   siteMetadata?: Maybe<SiteSiteMetadataFilterInput>;
+  port?: Maybe<IntQueryOperatorInput>;
+  host?: Maybe<StringQueryOperatorInput>;
   polyfill?: Maybe<BooleanQueryOperatorInput>;
   pathPrefix?: Maybe<StringQueryOperatorInput>;
   id?: Maybe<StringQueryOperatorInput>;
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -15,7 +15,7 @@ export const Layout: React.FC = ({ children }) => {
   }
 
   return (
-    <div className="tracking-wide transition-colors duration-75 dark:bg-gray-800 dark:text-gray-50">
+    <div className="tracking-wide transition-colors duration-500 dark:bg-gray-800 dark:text-gray-50">
       <header className="sticky top-0 left-0 right-0 z-100 bg-[hsla(0,0%,100%,0.3)] backdrop-filter backdrop-blur-sm dark:bg-[hsla(221,39%,11%,0.3)]">
         <Header />
       </header>
