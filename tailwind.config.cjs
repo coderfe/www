f1ef5207a18cd77182ab03fd2cb756e1856f98af
coderfee <coderfee@outlook.com>
2023-12-05 11:58:55 +0800

    icon 替换

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,15 +3,5 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#33b270',
-        'accent-1': '#ffca21',
-        'text-1': '#242f5a',
-        'bg-1': '#fdfdff',
-      },
-    },
-  },
   plugins: [require('@tailwindcss/typography')],
 };
