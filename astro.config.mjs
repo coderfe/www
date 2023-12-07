55662964a8004429ea2fd17bbb711b0e9b806802
coderfee <coderfee@outlook.com>
2023-12-07 17:59:36 +0800

    增加 Astro devtool bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://coderfee.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  integrations: [mdx(), sitemap(), tailwind(), react(), sentry(), spotlightjs()],
 });
