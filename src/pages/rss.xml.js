3ba7904e4c0bec952186af825128744af07b08c3
coderfee <coderfee@outlook.com>
2024-01-13 11:40:14 +0800

    fix: rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,21 +1,25 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
-export async function get(context) {
+export async function GET(context) {
   const posts = await getCollection('blog');
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
     items: posts.map((post) => {
-      const {tldr = '', date} = post.data;
+      const { tldr = '', date } = post.data;
       return {
         ...post.data,
         pubDate: date,
         description: tldr,
         link: `/blog/${post.slug}/`,
-      }
+        content: sanitizeHtml(parser.render(post.body)),
+      };
     }),
   });
 }
