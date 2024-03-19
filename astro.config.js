71de092290b239bb1b9694855400aed89bc7f754
coderfee <coderfee@outlook.com>
2024-03-19 08:50:36 +0800

    使用 shiki

--- astro.config.js
+++ astro.config.js
@@ -10,6 +10,14 @@ import spotlightjs from '@spotlightjs/astro';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://coderfee.com',
+  markdown: {
+    syntaxHighlight: 'shiki',
+    gfm: true,
+    shikiConfig: {
+      theme: 'vitesse-dark',
+      wrap: true,
+    },
+  },
   integrations: [
     mdx(),
     sitemap(),
