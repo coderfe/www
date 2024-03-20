2c0818b0e7b8eb5b5032404b29a347e2e49178cf
coderfee <coderfee@outlook.com>
2024-03-20 21:57:48 +0800

    ✨

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,18 +2,20 @@ import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
   // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    tldr: z.string(),
-    // Transform string to Date object
-    date: z
-      .string()
-      .or(z.date())
-      .transform((val) => new Date(val)),
-    heroImage: z.string().optional(),
-    tags: z.array(z.string()).optional(),
-    draft: z.boolean().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      tldr: z.string(),
+      // Transform string to Date object
+      date: z
+        .string()
+        .or(z.date())
+        .transform((val) => new Date(val)),
+      heroImage: z.string().optional(),
+      tags: z.array(z.string()).optional(),
+      draft: z.boolean().optional(),
+      cover: image().optional(),
+    }),
 });
 
 export const collections = { blog };
