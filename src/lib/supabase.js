b021802f3b5fe8bea42b82c41c4b51e4fb1b6805
coderfee <coderfee@outlook.com>
2024-04-20 11:43:41 +0800

    enable supabase RLS

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
-const supabaseKey = import.meta.env.PUBLIC_SUPABASE_KEY;
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseServiceKey = import.meta.env.PUBLIC_SUPABASE_SERVICE_KEY;
+export const supabase = createClient(supabaseUrl, supabaseServiceKey);
