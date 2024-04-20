5a5e9964b363f39d638ba03c0c80d99643f70d96
coderfee <coderfee@outlook.com>
2024-04-20 11:27:51 +0800

    :bug:

--- src/components/visitors/index.jsx
+++ src/components/visitors/index.jsx
@@ -3,9 +3,10 @@ import { supabase } from '@/lib/supabase.js';
 
 export function Visitors({ pathname }) {
   const [visitors, setVisitors] = useState(0);
+  const sulgPathname = slugify(pathname);
   useEffect(() => {
     async function getVisitors() {
-      let { data: visitor, error } = await supabase.from('visitor').select('*').eq('path', pathname);
+      let { data: visitor, error } = await supabase.from('visitor').select('*').eq('path', sulgPathname);
       const [currentPage] = visitor;
       if (!error) {
         setVisitors(currentPage.count);
@@ -13,18 +14,19 @@ export function Visitors({ pathname }) {
     }
 
     async function setVisitor() {
-      const { data, error } = await supabase.from('visitor')
-        .select('*')
-        .eq('path', pathname);
+      if (!sulgPathname) return;
+      const { data, error } = await supabase.from('visitor').select('*').eq('path', sulgPathname);
       if (!error) {
         if (data.length === 0) {
-          await supabase.from('visitor').insert({ path: pathname, count: 1 });
+          await supabase.from('visitor').insert({ path: sulgPathname, count: 1 });
         } else {
-          await supabase.from('visitor').update({ count: data[0].count + 1 }).eq('path', pathname);
+          await supabase
+            .from('visitor')
+            .update({ count: data[0].count + 1 })
+            .eq('path', sulgPathname);
         }
         getVisitors();
       }
-
     }
 
     setVisitor();
@@ -34,13 +36,14 @@ export function Visitors({ pathname }) {
 
   return (
     <div className="text-center text-sm space-x-4 pb-4">
-      <span>
-        👏欢迎，本页访问次数 {visitors}
-      </span>
-      <span>
-        ❤️
-      </span>
+      <span>👏欢迎，本页访问次数 {visitors}</span>
+      <span>❤️</span>
       <span className="text-gray-500">Powered By Supabase</span>
     </div>
   );
+}
+
+function slugify(str) {
+  if (!str || str === '/') return '-';
+  return str.replace(/^\/|\/$/g, '').replace(/\//g, '-');
 }
