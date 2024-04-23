7e47c4b898f7f78503b658ae48f09f6a4d61ff60
coderfee <coderfee@outlook.com>
2024-04-23 14:14:23 +0800

    :lipstick:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class'],
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   prefix: '',
   plugins: [require('@tailwindcss/typography')],
+  theme: {
+    fontFamily: {
+      sans: ['"LXGW WenKai Screen"', ...defaultTheme.fontFamily.sans],
+      mono: ['"Geist Mono"', 'Monolisa', ...defaultTheme.fontFamily.mono],
+    },
+  },
 };
