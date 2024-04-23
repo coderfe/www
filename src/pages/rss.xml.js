88df0f4f50fcce17a03bebddbccdc416c5a281d9
coderfee <coderfee@outlook.com>
2024-04-23 13:52:41 +0800

    fix rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,13 +4,14 @@ import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
   const collection = await getCollection('blog');
-  const posts = collection.sort(
-    ({ data: { date: dateA } }, { data: { date: dateB } }) => dateB.valueOf() - dateA.valueOf(),
-  );
+  const posts = collection
+    .filter(({ data: { draft = false } }) => !draft)
+    .sort(({ data: { date: dateA } }, { data: { date: dateB } }) => dateB.valueOf() - dateA.valueOf());
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
+    stylesheet: '/rss/styles.xsl',
     items: posts.map((post) => {
       const { tldr = '', date } = post.data;
       return {
