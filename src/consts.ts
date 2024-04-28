e3acef79f2e9b7d44c77ef3d37c44d7ef27159e4
coderfee <coderfee@outlook.com>
2024-04-28 12:02:09 +0800

    首页优化

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = 'CSpace';
+export const SITE_TITLE = "Coderfee's Blog";
 export const SITE_DESCRIPTION =
-  'coderfee CSpace 是一个关于前端开发、React、Vue、Vite、CSS 和 JavaScript 的博客网站，一些技术文章，以及一些生活感悟。';
+  "Coderfee'Blog coderfee.com 是一个关于编程、AI 应用、读书笔记和个人随笔的博客。一起探索编程之美、AI 之智、读书之乐和写作之趣吧！";
