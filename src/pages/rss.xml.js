120cf5a5f3926bd4ff2af122c2c7d8354b7bb6d1
coderfee <coderfee@outlook.com>
2024-10-26 09:29:36 +0800

    :arrow_up:

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,9 @@
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
+import MarkdownIt from 'markdown-it';
+import sanitizeHtml from 'sanitize-html';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const collection = await getCollection('blog');
@@ -11,7 +14,7 @@ export async function GET(context) {
     title: SITE_TITLE,
     description: `${SITE_DESCRIPTION} feedId:52340201851637784+userId:55812696340985856`,
     site: context.site,
-    stylesheet: '/rss/styles.xsl',
+    // stylesheet: "/rss/styles.xsl",
     items: posts.map((post) => {
       const { tldr = '', date } = post.data;
       return {
@@ -19,6 +22,9 @@ export async function GET(context) {
         pubDate: date,
         description: tldr,
         link: `/blog/${post.slug}/`,
+        content: sanitizeHtml(parser.render(post.body), {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+        }),
       };
     }),
   });
