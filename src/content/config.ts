1cef4b2e700edd1882c93a7026a28d91ee46e170
coderfee <coderfee@outlook.com>
2025-03-08 20:57:24 +0800

    feat: add newsletter collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -18,4 +18,21 @@ const blog = defineCollection({
     }),
 });
 
-export const collections = { blog };
+const newsletter = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      date: z
+        .string()
+        .or(z.date())
+        .transform((val) => new Date(val)),
+      issue: z.number(),
+      draft: z.boolean().optional(),
+      cover: image().optional().or(z.string().optional()),
+    }),
+});
+
+export const collections = { blog, newsletter };
