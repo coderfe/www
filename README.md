6af161cda7f974babefeaec81a52c1c06dc3b5ce
coderfee <coderfee@outlook.com>
2025-03-09 16:04:42 +0800

    feat: update blog title casing and improve background components

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Coderfee's Blog
+# coderfee's blog
 
 ![Astro](https://img.shields.io/badge/Astro-FF5D01?style=for-the-badge&logo=astro&logoColor=white)
 ![React](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB)
--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
-export const SITE_TITLE = "coderfee's Blog";
+export const SITE_TITLE = "coderfee's blog";
 export const SITE_DESCRIPTION =
-  "coderfee's Blog 是一个关于编程、AI 应用、读书笔记和个人随笔的博客。一起探索编程之美、AI 之智、读书之乐和写作之趣吧！";
+  "coderfee's blog 是一个关于编程、AI 应用、读书笔记和个人随笔的博客。一起探索编程之美、AI 之智、读书之乐和写作之趣吧！";
 
 export type Social = {
   name: string;
